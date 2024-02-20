9fe579101bff495b8e506abf9f4342f37e77ab17
Vinícius Xavier <viniciusxavier2212@gmail.com>
2024-02-20 19:06:15 -0300

    fix: adds missing "years" interpolation

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -9,7 +9,7 @@
         <h1 class="gradient-underline sm:text-5xl text-4xl w-fit inline font-title">
           {{ $t('hey') }}
         </h1>
-        <div v-html="$t('aboutContent')" />
+        <div v-html="$t('aboutContent', { years })" />
       </div>
       <div class="flex justify-center flex-1">
         <ClientOnly>
@@ -27,11 +27,16 @@
 <script setup lang="ts">
 const startDate = new Date(2020, 3, 1)
 const years = ref('');
+const { locale } = useI18n()
 
 const updateYears = () => {
   const now = Date.now()
-  years.value = Intl.NumberFormat('pt-BR', { maximumFractionDigits: 8, minimumFractionDigits: 8}).format(((now - startDate.getTime()) / (1000 * 60 * 60 * 24 * 365)))
+  years.value = Intl.NumberFormat(locale.value, { maximumFractionDigits: 8, minimumFractionDigits: 8}).format(((now - startDate.getTime()) / (1000 * 60 * 60 * 24 * 365)))
 }
+
+watch(locale, () => {
+  updateYears()
+})
 
 onMounted(() => {
   updateYears()
