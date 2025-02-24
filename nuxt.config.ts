95ef328172f5575eb4f2a4b660a111004addd250
Vinícius Xavier <viniciusxavier2212@gmail.com>
2025-02-24 18:29:15 -0300

    build: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,36 +3,40 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", '@vueuse/nuxt', 'nuxt-icon', '@nuxtjs/i18n', '@nuxtjs/robots', 'nuxt-gtag'],
   css: ["~/assets/css/main.css"],
+
   i18n: {
-    fallbackLocale: 'en',
+    defaultLocale: 'en',
     baseUrl: 'https://viniciusxavier.dev/',
     locales: [
       {
-        iso: 'en-US',
+        language: 'en-US',
         code: 'en',
         name: 'EN',
         file: 'en-US.ts',
       },
       {
-        iso: 'pt-BR',
+        language: 'pt-BR',
         code: 'pt',
         name: 'PT',
         file: 'pt-BR.ts',
       }
     ],
     langDir: 'i18n',
-    lazy: true,
     strategy: 'no_prefix',
     compilation: {
       strictMessage: false,
     },
   },
+
   routeRules: {
     '/hellos/**': { proxy: 'https://hellos-kappa.vercel.app' },
     '/': { prerender: true }
   },
+
   gtag: {
     enabled: process.env.NODE_ENV === 'production',
     id: 'G-9SPFP9JER1'
-  }
+  },
+
+  compatibilityDate: '2025-02-24'
 });
