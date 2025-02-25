3279885b1e176e5d810fc86c6367ca6f6d32b9fd
Vinícius <viniciusxavier2212@gmail.com>
2025-02-25 01:09:39 -0300

    refactor: remove unnecessary v-html

--- components/IntroSection.vue
+++ components/IntroSection.vue
@@ -1,22 +1,15 @@
 <!-- eslint-disable vue/no-v-html -->
 <template>
   <section
-    class="flex justify-center sm:justify-start items-center sm:text-5xl text-4xl sm:leading-tight leading-tight h-screen w-full"
-  >
+    class="flex justify-center sm:justify-start items-center sm:text-5xl text-4xl sm:leading-tight leading-tight h-screen w-full">
     <h1 class="-mt-40 mx-10 md:mx-36 sm:mx-12 font-title">
       <p>{{ $t('hi') }}</p>
       <p>
-        {{ $t('greetings') }} <a
-          class="link"
-          href="#about"
-        >{{ $t('name') }}</a>
+        {{ $t('greetings') }} <a class="link" href="#about">{{ $t('name') }}</a>
       </p>
       <p>
-        <span v-html="$t('doerOf')" />
-        <a
-          class="link"
-          href="#things"
-        >{{ $t('things') }}</a>
+        {{ $t('doerOf') }}
+        <a class="link" href="#things">{{ $t('things') }}</a>
         <span v-html="$t('forinternet', { years })" />
       </p>
     </h1>
